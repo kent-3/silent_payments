629c96699778c67b78350308163b4615d003a4da
kent-3 <100624004+kent-3@users.noreply.github.com>
2025-03-20 21:28:30 -0400

    first commit

---
+++ example/silent_payments_example.dart
@@ -0,0 +1,6 @@
+import 'package:silent_payments/silent_payments.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/silent_payments.dart
@@ -0,0 +1,6 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/silent_payments_base.dart';
